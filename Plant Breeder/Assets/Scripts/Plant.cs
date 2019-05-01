8046085457440b516af1b8a03ac5205b06e713cf
Joao Rabello <rabellois@hotmail.com>
2019-05-01 03:37:31 -0300

    Sistema da Estufa
    - Criado o script da planta que controla o ganho de felicidade e armazena os status da planta
    - Adicionado um placeholder de 3 imagens verdes para a planta

---
+++ Plant Breeder/Assets/Scripts/Plant.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour {
+
+    public float plantValue;
+    public float timeToEarnHappiness;
+    private float timer;
+
+    public float happiness;
+    public float water;
+    public float sun;
+    public float health;
+	
+	void Start () {
+        happiness = 0f;
+        water = 0f;
+        sun = 0f;
+        health = 0f;
+	}
+	
+	void Update () {
+		
+        if(timer <= timeToEarnHappiness)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0f;
+            happiness += plantValue;
+            Debug.Log("Felicidade: " + happiness.ToString("F0"));
+        }
+	}
+}
