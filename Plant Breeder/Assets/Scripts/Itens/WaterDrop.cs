7b0d9ab407a30d8119f3c9eb1fec9955fe17960b
Joao Rabello <rabellois@hotmail.com>
2019-05-07 00:09:41 -0300

    Protótipo de Regador
    - Criado um regador que instancia gotas de água sem utilidade e um botão para ativá-lo

---
+++ Plant Breeder/Assets/Scripts/Itens/WaterDrop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDrop : MonoBehaviour {
+
+    float timer = 0f;
+	
+	void Update () {
+		if(timer <= 1f)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ Plant Breeder/Assets/Scripts/Itens/WateringCan.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WateringCan : MonoBehaviour {
+
+    Camera cam;
+    public GameObject gota;
+    float timer = 0f;
+    float timeToSpawn = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+
+        cam = Camera.main;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if(timer <= timeToSpawn)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer = 0f;
+                Instantiate(gota, new Vector3(transform.position.x + Random.Range(-0.4f,0.4f), transform.position.y), Quaternion.identity);
+            }
+        }
+
+	}
+}
